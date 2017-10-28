49083ecc0569a2a48a06006634b7038928b0de40
cyh <cyh0214@gmail.com>
2017-10-29 00:32:07 +0900

    initial

--- src/main/java/com/java8/exam/lambda/lambdaExam.java
+++ src/main/java/com/java8/exam/lambda/lambdaExam.java
@@ -1,10 +1,21 @@
 package com.java8.exam.lambda;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class lambdaExam {
 
     public static void main(String args[]){
         // lambda 표현식
         System.out.println("lambda expression");
+        
+        List<String> stringList = Arrays.asList("A" , "B" , "C");
+        
+        Collections.sort(stringList , (o1 , o2) -> o2.compareTo(o1));
+        
+        stringList.forEach(System.out::println);
+        
     }
 
 }
