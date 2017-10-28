2189f13f414417bded04d107533e967dd8a8eb07
cyh <cyh0214@gmail.com>
2017-10-28 22:57:29 +0900

    initial

---
+++ src/main/java/com/java8/exam/lambda/lambdaExam.java
@@ -0,0 +1,10 @@
+package com.java8.exam.lambda;
+
+public class lambdaExam {
+
+    public static void main(String args[]){
+        // lambda 표현식
+        System.out.println("lambda expression");
+    }
+
+}
