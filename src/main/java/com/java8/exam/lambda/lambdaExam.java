91149a9a90bd46cb1bc83e0a0a1de95331b043ac
cyh <cyh0214@gmail.com>
2017-10-29 20:27:43 +0900

    Signed-off-by: cyh <cyh0214@gmail.com>

--- src/main/java/com/java8/exam/lambda/lambdaExam.java
+++ src/main/java/com/java8/exam/lambda/lambdaExam.java
@@ -2,7 +2,9 @@ package com.java8.exam.lambda;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class lambdaExam {
 
@@ -15,6 +17,13 @@ public class lambdaExam {
         Collections.sort(stringList , (o1 , o2) -> o2.compareTo(o1));
         
         stringList.forEach(System.out::println);
+        
+        Map<String , String> map = new HashMap<>();
+        map.put("A", "AA");
+        map.put("B", "BB");
+        
+        map.forEach( (k , v) -> System.out.println("key:"+k+" value:"+v));
+        
         
     }
 
---
+++ src/main/java/com/java8/exam/lambda/streamExam.java
@@ -0,0 +1,57 @@
+package com.java8.exam.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class streamExam {
+	
+	public static void main(String args[]){
+		
+		List<String> names = Arrays.asList("A" , "B" , "C");
+		
+		names.stream().filter( x -> !x.contains("A")).forEach( x -> System.out.println(x) );
+		
+		List<Person> persons = Arrays.asList(
+                new Person("a", 10),
+                new Person("b", 20),
+                new Person("c", 30)
+        );
+		
+		List<Integer> numbers = Arrays.asList(1 , 2 , 3 , 4 , 5);
+		
+		Optional<Integer> result = numbers.stream().reduce( (x,y) -> Math.max(x, y));
+
+		
+	}
+	
+	
+}
+
+class Person{
+
+	private String name;
+	private int age;
+	
+	public Person(String name , int age){
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
