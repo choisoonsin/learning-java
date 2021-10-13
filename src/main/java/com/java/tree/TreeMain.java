d8b3194be4df0ee15fc307d06e4586f908956f62
choiyounghoon <cyh0214@gmail.com>
2021-10-13 16:25:45 +0900

    add Tree algorithm

---
+++ src/main/java/com/java/tree/TreeMain.java
@@ -0,0 +1,40 @@
+package com.java.tree;
+
+class Node {
+
+    private String alphabet;
+    private Node leftNode;
+    private Node rightNode;
+    
+    public String getAlphabet() {
+        return alphabet;
+    }
+    public void setAlphabet(String alphabet) {
+        this.alphabet = alphabet;
+    }
+    public Node getLeftNode() {
+        return leftNode;
+    }
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+    public Node getRightNode() {
+        return rightNode;
+    }
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+    
+}
+
+class Tree {
+
+}
+
+public class TreeMain {
+    public static void main(String args[]) {
+        System.out.println("Tree algorithm");
+
+        String givenValue = "ABCDEFGH";
+    }
+}
